04af7981af441ef5a3b22a61e97d15360b0fc1d7
harveydb <harveydb@debianharv>
2022-07-05 22:28:54 +0200

    This commit add the code when explains the shallow copy, assign() and creat()

--- main.js
+++ main.js
@@ -12,11 +12,56 @@ const juan = {
 // console.log(Object.getOwnPropertyNames(juan));
 // console.log(Object.entries(juan));
 
-console.log(Object.getOwnPropertyDescriptors(juan));
 
-Object.defineProperty(juan,"pruebaNASA", {
-  value: "extraterrestres",
+Object.defineProperty(juan,"navigator", {
+  value: "chrome",
   writable: true,
+  enumerable: false,
+  configurable: true
+})
+
+
+Object.defineProperty(juan,"editor", {
+  value: "VSCode",
+  writable: false,
   enumerable: true,
   configurable: true
 })
+
+Object.defineProperty(juan,"terminal", {
+  value: "WSL",
+  writable: true,
+  enumerable: true,
+  configurable: false
+})
+
+Object.defineProperty(juan,"pruebaNASA", {
+  value: "extraterrestres",
+  writable: false,
+  enumerable: false,
+  configurable: false
+})
+
+console.log(Object.getOwnPropertyDescriptors(juan));
+
+// Shallow object
+const obj1 = {
+  a: 'a',
+  b: 'b',
+  c: {
+    d: 'd',
+    e: 'e'
+  }
+}
+
+const obj2 = {};
+
+for(prop in obj1) {
+  obj2[prop] = obj1[prop] 
+}
+
+// Metodo estatico assign de Object
+const obj3 = Object.assign({}, obj1);
+
+// Metodo estatico create de Object
+const obj4 = Object.create(obj1)
